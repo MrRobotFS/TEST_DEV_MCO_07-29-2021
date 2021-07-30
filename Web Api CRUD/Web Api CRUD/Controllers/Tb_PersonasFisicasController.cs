c5e6b16106ab85a6b47dc0b4bad8a690dad8344b
Mr.Robot <max-carrion@hotmail.com>
2021-07-29 20:13:34 -0500

    [ADD] Bring the model data to the controller

---
+++ Web Api CRUD/Web Api CRUD/Controllers/Tb_PersonasFisicasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_Api_CRUD.Controllers
+{
+    public class Tb_PersonasFisicasController : Controller
+    {
+        // GET: Tb_PersonasFisicas
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web Api CRUD/Web Api CRUD/Models/ViewModels/ListTb_PersonasFisicasViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_Api_CRUD.Models.ViewModels
+{
+    public class ListTb_PersonasFisicasViewModel
+    {
+        //Data type source https://stackoverflow.com/questions/425389/c-sharp-equivalent-of-sql-server-datatypes 
+
+        public int IdPersonaFisica { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public DateTime FechaActualizacion { get; set; }
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public string RFC { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public int UsuarioAgrega { get; set; }
+        public Boolean Activo { get; set; }
+    }
+}
