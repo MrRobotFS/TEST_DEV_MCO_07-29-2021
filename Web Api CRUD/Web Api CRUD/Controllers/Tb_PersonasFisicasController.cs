28f0eb4f90a4fbc908a7f69821eb59e493772617
Mr.Robot <max-carrion@hotmail.com>
2021-07-29 23:11:20 -0500

    [ADD] Create View
    I create the method for post the data and the view layout, but now I need to connect with the submit button

--- Web Api CRUD/Web Api CRUD/Controllers/Tb_PersonasFisicasController.cs
+++ Web Api CRUD/Web Api CRUD/Controllers/Tb_PersonasFisicasController.cs
@@ -37,5 +37,48 @@ namespace Web_Api_CRUD.Controllers
 
             return View(lst);
         }
+
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+
+        // An overload is applied to write with the POST method.
+        [HttpPost]
+        public ActionResult Nuevo(Tb_PersonasFisicasViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (masterEntities db = new masterEntities())
+                    {
+                        var oTabla = new Tb_PersonasFisicas();
+                        oTabla.IdPersonaFisica = model.IdPersonaFisica;
+                        oTabla.FechaRegistro = model.FechaNacimiento;
+                        oTabla.Nombre = model.Nombre;
+                        oTabla.ApellidoPaterno = model.ApellidoPaterno;
+                        oTabla.ApellidoMaterno = model.ApellidoMaterno;
+                        oTabla.RFC = model.RFC;
+                        oTabla.FechaNacimiento = model.FechaNacimiento;
+                        oTabla.UsuarioAgrega = model.UsuarioAgrega;
+                        oTabla.Activo = model.Activo;
+
+                        //Then add the data into the DB
+                        db.Tb_PersonasFisicas.Add(oTabla);
+                        db.SaveChanges();
+                    }
+
+                }
+
+                return Redirect("Tb_PersonasFisicas/");
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
     }
 }
---
+++ Web Api CRUD/Web Api CRUD/Models/ViewModels/Tb_PersonasFisicasViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web_Api_CRUD.Models.ViewModels
+{
+    public class Tb_PersonasFisicasViewModel
+    {
+        public int IdPersonaFisica { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de Registro")]
+        public DateTime FechaRegistro { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Nombre")]
+        public string Nombre { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Apellido Paterno")]
+        public string ApellidoPaterno { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Apellido Materno")]
+        public string ApellidoMaterno { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "RFC")]
+        public string RFC { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de Nacimiento")]
+        public DateTime FechaNacimiento { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Quien Agrega?")]
+        public int UsuarioAgrega { get; set; }
+        [Required]
+        [Display(Name = "Status")]
+        public Boolean Activo { get; set; }
+    }
+}
