7b1a3ee9233b17c4cb0ae7334ae807c0e0ff1ffa
Mr.Robot <max-carrion@hotmail.com>
2021-07-29 20:40:22 -0500

    [END] Display data on the view side
    The data was mapped with the Entity Framework, after that I created a class view for define the kind of the value with the DB field, after I created the Controller that list the variable with the DB value, and finally I called the view model into the view side and it displays the a table with the DB fields, now I have to create data for see if it works lol

--- Web Api CRUD/Web Api CRUD/Controllers/Tb_PersonasFisicasController.cs
+++ Web Api CRUD/Web Api CRUD/Controllers/Tb_PersonasFisicasController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+//Bring the class for map the data with the Entity element
+using Web_Api_CRUD.Models;
+using Web_Api_CRUD.Models.ViewModels;
+
 namespace Web_Api_CRUD.Controllers
 {
     public class Tb_PersonasFisicasController : Controller
@@ -11,7 +15,27 @@ namespace Web_Api_CRUD.Controllers
         // GET: Tb_PersonasFisicas
         public ActionResult Index()
         {
-            return View();
+            List<ListTb_PersonasFisicasViewModel> lst;
+            using (masterEntities db = new masterEntities())
+            {
+                 lst = (from d in db.Tb_PersonasFisicas
+
+                           select new ListTb_PersonasFisicasViewModel
+                           {
+                               IdPersonaFisica = d.IdPersonaFisica,
+                               FechaRegistro = (DateTime)d.FechaRegistro,
+                               FechaActualizacion = (DateTime)d.FechaActualizacion,
+                               Nombre = d.Nombre,
+                               ApellidoPaterno = d.ApellidoPaterno,
+                               ApellidoMaterno = d.ApellidoMaterno,
+                               RFC = d.RFC,
+                               FechaNacimiento = (DateTime)d.FechaNacimiento,
+                               UsuarioAgrega = (int)d.UsuarioAgrega,
+                               Activo = (bool)d.Activo,
+                           }).ToList();
+            }
+
+            return View(lst);
         }
     }
 }
